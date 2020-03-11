0698cab1eaed0d1bef3550e7daf7dd251d3f9dc9
MurilloWolf <mwolfcavalheiro@gmail.com>
2020-03-11 16:15:05 -0300

    alter Resolucao Canibais

--- exercicios/Canibais/resolucao.pl
+++ exercicios/Canibais/resolucao.pl
@@ -1,11 +1,11 @@
 %Vetor Inicial [3,3,0,0,e].
 %Vetor Final [0,0,3,3,d].
 %Disposição no vetor 
-[
-    ContCanibais_Esquerda, ContPadres_Esquerda,
-    ContCanibais_Direita, ContPadres_Direita,
-    Pos_Barco
-]
+%[
+%    ContCanibais_Esquerda, ContPadres_Esquerda,
+%    ContCanibais_Direita, ContPadres_Direita,
+%    Pos_Barco
+%]
 
 inc(Numero, Incrementado): - Incrementado is Numero + 1. 
 dec(Numero, Incrementado): - Incrementado is Numero - 1. 
@@ -33,4 +33,47 @@ pode_ir([CCE, CPE, CCD,CPD, B ],[CCE, NCPE, CCD, NCPD, NB]):-
 pode_ir([CCE, CPE, CCD,CPD, B ],[CCE, CPE, NCCD, CPD, NB]):-
     dec(CCE,NCCE), inc(CCD, NCCD), oposto(B,NB).
 
-permitido([CCD,CPE, CCD, CPD, B]):- CCD <= CPD, CCE <=CPE.
+permitido([CCE,CPE, CCD, CPD, B]):- (CCD <= CPD ; CPD = 0 ) ,  ( CCE <= CPE ; CPE = 0), .
+
+
+%%BUSCA EM PROFUNDIDADE
+rota(Origem, Destino, Cam):-
+	 rota(Origem, Destino, [Origem], Cam).
+
+rota(Destino, Destino, Cam, Cam).
+
+rota(Origem, Destino, CamAux, Cam):-
+	 pode_ir(Origem, Vizinha),
+	 permitido(Vizinha),
+	 not(pertence(Vizinha,CamAux)),
+	 rota(Vizinha, Destino, [Vizinha|CamAux], Cam).
+
+pertence(Elem,[Elem|_]):-!.
+pertence(Elem,[_|Cauda]):- pertence(Elem, Cauda).
+
+resolve_fazendeiro(EstIni, EstFinal):- rota(EstIni, EstFinal, Cam), imprime2(Cam).
+
+imprime([]).
+imprime([C|Cauda]):-imprime(Cauda), nl, write(C).
+
+imprime2([]).
+imprime2([_]).
+imprime2([C1,C2|Cauda]):- imprime2([C2|Cauda]),
+			  nl, mostra(C1,C2).
+
+mostra([CCD,CPE,CCD,CPD, B],[NCCE,NCPE,NCCD,NCPE, NB]):-write('Foram um padre e um canibal da'), ext(F,Texto1),
+			write(Texto1), write(' para a'), ext(NF,Texto2),
+			write(Texto2).
+
+mostra([CCD,CPE,CCD,CPD, B],[NCCE,CPE,NCCD,CPE, NB]):-write('Foram'), quantidade(CCD,NCCD,Qtd), write(Qtd)  write(' canibal(is) da'), ext(F,Texto1),
+			write(Texto1), write(' para a'), ext(NF,Texto2),
+			write(Texto2).
+
+mostra([CCD,CPE,CCD,CPD, B],[CCE,NCPE,CCD,NCPD, NB]):-write('Foram'), quantidade(CPD,NCPD,Qtd), write(Qtd)  write(' padre(s) da'), ext(F,Texto1),
+			write(Texto1), write(' para a'), ext(NF,Texto2),
+			write(Texto2).
+
+
+quantidade(C1, C2 , qtd):- (C1 > C2, qtd = C1-C2 )! ; qtd = C2 - C1. 
+ext(e,esquerda).
+ext(d,direita).
