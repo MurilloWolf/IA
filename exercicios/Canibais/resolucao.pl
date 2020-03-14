d6a24e8baf1749002f04ed79b290907065d14ff0
MurilloWolf <mwolfcavalheiro@gmail.com>
2020-03-14 01:00:31 -0300

    fix side of boat in pode_ir

--- exercicios/Canibais/resolucao.pl
+++ exercicios/Canibais/resolucao.pl
@@ -7,33 +7,39 @@
 %    Pos_Barco
 %]
 
-inc(Numero, Incrementado):- Incrementado is Numero + 1.
-dec(Numero, Incrementado):- Incrementado is Numero - 1.
+inc(Numero, Qtd_de_Incremento, Retorno):- Retorno is Numero + Qtd_de_Incremento.
+dec(Numero, Qtd_de_Decremento, Retorno):- Retorno is Numero - Qtd_de_Decremento.
 oposto(d,e).
 oposto(e,d).
 
 
 %Transporta 1 padre e 1 canibal para o outro lado
 pode_ir([CCE, CPE, CCD,CPD, B ],[NCCE, NCPE, NCCD, NCPD, NB]):-
-    dec(CCE,NCCE), dec(CPE,NCPE), inc(CCD,NCCD), inc(CPD, NCPD), oposto(B,NB).
+	%Vai fazer isso se o barco estiver na esquerda
+    ((dec(CCE, 1 , NCCE), dec(CPE, 1,NCPE), inc(CCD, 1,NCCD), inc(CPD,1 , NCPD), oposto(B,NB)), B = e);
+	(dec(CCD, 1 , NCCD), dec(CPD, 1,NCPD), inc(CCE, 1,NCCE), inc(CPE,1 , NCPE), oposto(B,NB)).
 
 %Transporta 0 padre e 2 canibal para o outro lado
 pode_ir([CCE, CPE, CCD,CPD, B ],[NCCE, CPE, NCCD, CPD, NB]):-
-    dec(CCE,CCEAux), dec(CCEAux, NCCE), inc(CCD, CCDAux), inc(CCDAux, NCCD), oposto(B,NB).
+    ((dec(CCE, 2, NCCE), inc(CCD, 2, NCCD), oposto(B,NB)),B = e );
+	(dec(CCD, 2, NCCD), inc(CCE, 2, NCCE), oposto(B,NB)).
 
 %Transporta 0 padre e 2 canibal para o outro lado
 pode_ir([CCE, CPE, CCD,CPD, B ],[CCE, NCPE, CCD, NCPD, NB]):-
-    dec(CPE,CPEAux), dec(CPEAux, NCPE), inc(CPD, CPDAux), inc(CPDAux, NCPD), oposto(B,NB).
+    ((dec(CPE,2, NCPE), inc(CPD, 2, NCPD), oposto(B,NB)), B = e );
+	((dec(CPD,2, NCPD), inc(CPE, 2, NCPE), oposto(B,NB)).
 
 %Transporta 1 padre
 pode_ir([CCE, CPE, CCD,CPD, B ],[CCE, NCPE, CCD, NCPD, NB]):-
-    dec(CPE,NCPE), inc(CPD, NCPD), oposto(B,NB).
+    ((dec(CPE,1 ,NCPE), inc(CPD, 1, NCPD), oposto(B,NB)), B = e )
+	((dec(CPD,1 ,NCPD), inc(CPE, 1, NCPE), oposto(B,NB)).
 
 %Transporta 1 canibal
 pode_ir([CCE, CPE, CCD,CPD, B ],[NCCE, CPE, NCCD, CPD, NB]):-
-    dec(CCE,NCCE), inc(CCD, NCCD), oposto(B,NB).
+    ((dec(CCE, 1 , NCCE), inc(CCD,1 , NCCD), oposto(B,NB)), B = e);
+	((dec(CCD, 1 , NCCD), inc(CCE,1 , NCCE), oposto(B,NB)).
 
-permitido([CCE,CPE, CCD, CPD, B]):- (CCD =< CPD ; CPD = 0 ) ,  (CCE =< CPE ; CPE = 0).
+permitido([CCE,CPE, CCD, CPD, _]):- (CCD =< CPD ; CPD = 0 ) ,  (CCE =< CPE ; CPE = 0).
 
 
 %%BUSCA EM PROFUNDIDADE
